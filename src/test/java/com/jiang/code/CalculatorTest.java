7dd210b115f1ab4e753558b8ea1e520204228341
core.editor <jiangjianglovezhou@gmail.com>
2021-12-26 17:22:11 +1100

    fix test debug

--- src/test/java/com/jiang/code/CalculatorTest.java
+++ src/test/java/com/jiang/code/CalculatorTest.java
@@ -7,7 +7,7 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class CalculatorTest {
     Calculator tester = new Calculator();
-    int[] testResult = {0, 1};
+    int[] testResult = {1, 1};
     int[] imgBundles = {5, 10};
     double[] imgPrices = {450, 800};
     Format imgFormat = new Format("IMG", imgBundles, imgPrices);
@@ -21,6 +21,6 @@ class CalculatorTest {
     //test calculate function
     @Test
     void testCalculate() {
-        assertEquals(testResult, tester.calculate(10, imgFormat.getBundles()));
+        assertArrayEquals(testResult, tester.calculate(11, imgFormat.getBundles()));
     }
 }
